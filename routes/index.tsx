07525760e5f200cdd8a4f174f3bb073e4cc7ddfe
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-09-17 13:25:52 +0100

    feat: better desktop styling

--- routes/index.tsx
+++ routes/index.tsx
@@ -95,12 +95,11 @@ const projects: ProjectProps[] = shuffle([
     name: "RightClickHarvest",
     type: ProjectType.Minecraft,
     link: "https://www.curseforge.com/minecraft/mc-mods/rightclickharvest",
-    image:
-      "https://github.com/JamCoreModding/RightClickHarvest/blob/1.19.x/demo.gif?raw=true",
+    image: "/showcase/rightclickharvest.mp4",
     description: "A mod that allows you to harvest crops with right-click.",
   },
   {
-    name: "Totally Balanced Bone Drops",
+    name: "TBBD",
     type: ProjectType.Minecraft,
     link:
       "https://www.curseforge.com/minecraft/mc-mods/totally-balanced-bone-drops",
@@ -160,7 +159,7 @@ const projects: ProjectProps[] = shuffle([
     name: "'Sock's Storms'",
     type: ProjectType.Commission,
     link: "",
-    image: "https://i.imgur.com/sxUOKvA.gif",
+    image: "/showcase/storms.mp4",
     description:
       "A mod where random, crazy storms happen every few minutes, for SocksFor1.",
   },
@@ -191,31 +190,27 @@ function shuffle(a: any[]) {
 export default function Home() {
   return (
     <div
-      class={tw
-        `h-full min-h-screen bg-gray-200 flex flex-col p-6 md:px-12 md:py-6 font-roboto`}
+      class={tw`h-full min-h-screen bg-gray-200 flex flex-col p-6 md:px-12 md:py-6 font-roboto`}
     >
       <div
-        class={tw`flex flex-col md:flex-row md:items-center`}
+        class={tw`flex flex-col text-center md:flex-row md:items-center`}
       >
-        <p class={tw`text(2xl orange-500)`}>Jamalam</p>
-        <p class={tw`text-xl font-light pt-6 md:pl-24 md:pt-0`}>
+        <p class={tw`text(3xl orange-500) md:text-5xl`}>Jamalam</p>
+        <p class={tw`text-2xl md:text-3xl font-light pt-6 md:pl-24 md:pt-0`}>
           Developer of all manner of things.
         </p>
       </div>
 
-      <div class={tw`flex flex-col md:flex-row pt-16`}>
+      <div class={tw`flex flex-col md:flex-row pt-16 md:pt-6`}>
         <div
-          class={tw`flex flex-col`}
+          class={tw`flex flex-col items-stretch md:w-3/4 md:flex-row md:flex-wrap`}
         >
-          <p class={tw`text-xl font-light pb-2`}>Notable Projects</p>
-          <div class={tw`flex flex-col items-stretch md:flex-row md:flex-wrap`}>
-            {projects.map((project) => <Project {...project} />)}
-          </div>
+          {projects.map((project) => <Project {...project} />)}
         </div>
         <div class={tw`flex-grow`} />
-        <div class={tw`flex flex-col md:w-2/3`}>
+        <div class={tw`flex flex-col md:w-1/3`}>
           <SidebarSection title="About">
-            <p class={tw`text-lg font-light`}>
+            <p class={tw`md:text-lg md:text-2xl font-light`}>
               Hey! I'm Jamalam, or James, a developer from the UK. I primarily
               work on Minecraft, but I'm also interested in web development,
               amongst other things. I am open to any opportunities for custom
@@ -226,34 +221,38 @@ export default function Home() {
             </p>
           </SidebarSection>
           <SidebarSection title="Skills">
-            <ul class={tw`md:pl-2 list-disc`}>
+            <ul class={tw`md:pl-8 text-lg md:text-2xl list-disc`}>
               <li>
-                <p class={tw`text-lg font-light`}>Fabric & Quilt Modding</p>
+                <p class={tw`font-light`}>
+                  Fabric & Quilt Modding
+                </p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>
+                <p class={tw`font-light`}>
                   Multiplatform Modding (Architectury)
                 </p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>Web Development (fresh)</p>
+                <p class={tw`font-light`}>
+                  Web Development (fresh)
+                </p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>TailwindCSS</p>
+                <p class={tw`font-light`}>TailwindCSS</p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>
+                <p class={tw`font-light`}>
                   Discord Bot Development (Kord Extensions)
                 </p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>TypeScript</p>
+                <p class={tw`font-light`}>TypeScript</p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>Kotlin</p>
+                <p class={tw`font-light`}>Kotlin</p>
               </li>
               <li>
-                <p class={tw`text-lg font-light`}>Java</p>
+                <p class={tw`font-light`}>Java</p>
               </li>
             </ul>
           </SidebarSection>
@@ -261,7 +260,7 @@ export default function Home() {
             <div class={tw`flex flex-col`}>
               {links.map((link) => (
                 <a
-                  class={tw`text(lg orange-500) font-light`}
+                  class={tw`text(lg orange-500) md:text-2xl font-light`}
                   href={link.url}
                   target="_blank"
                 >
@@ -274,7 +273,7 @@ export default function Home() {
       </div>
 
       <div class={tw`pb-4 pt-6 flex justify-center`}>
-        <p class={tw`text-base font-extralight`}>
+        <p class={tw`text-base md:text-2xl font-extralight`}>
           Copyright © {new Date().getFullYear()} Jamalam.
         </p>
       </div>
@@ -295,8 +294,19 @@ function Project(props: ProjectProps) {
     <div
       class={tw`flex flex-col w-full md:w-1/3 pr-2 pl-0 py-2 md:pr-4`}
     >
-      <div class={tw`h-full p-4 rounded border-solid border-2 border-gray-300`}>
-        <div class={tw`flex justify-center`}>
+      <div
+        class={tw`h-full p-4 rounded border-solid border-2 border-gray-400 md:text-2xl`}
+      >
+        <a
+          class={tw`font-light h-8 md:text-3xl ${
+            !(props.link == "") ? "text-orange-500" : ""
+          }`}
+          href={props.link == "" ? undefined : props.link}
+          target="_blank"
+        >
+          {props.name}
+        </a>
+        <div class={tw`flex justify-center md:pt-4`}>
           <a href={props.image} target="_blank">
             <img
               loading="lazy"
@@ -308,17 +318,7 @@ function Project(props: ProjectProps) {
         </div>
 
         <div class={tw`flex flex-row pt-4`}>
-          <a
-            class={tw`font-light ${
-              !(props.link == "") ? "text-orange-500" : ""
-            }`}
-            href={props.link == "" ? undefined : props.link}
-            target="_blank"
-          >
-            {props.name}
-          </a>
-          <div class={tw`flex-grow`} />
-          <p class={tw`font-light pl-2 md:pl-0`}>{props.type}</p>
+          <p class={tw`font-light text-gray-600`}>{props.type}</p>
         </div>
 
         <p class={tw`pt-4 font-light`}>{props.description}</p>
@@ -336,7 +336,7 @@ function SidebarSection(props: SidebarSectionProps) {
   return (
     <details>
       <summary
-        class={tw`text-xl font-light cursor-pointer select-none`}
+        class={tw`text-xl md:text-3xl font-light cursor-pointer select-none`}
       >
         {" " + props.title}
       </summary>
