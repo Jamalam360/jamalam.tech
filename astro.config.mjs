35084c91624b2cc2f7df1897dfa3e1c00413daaa
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-09-24 11:06:57 +0100

    chore: deploy on gh pages for preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
-import deno from "@astrojs/deno";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "jamalam360.github.io",
   integrations: [tailwind(), sitemap(), preact(), image(), prefetch()],
-  output: "server",
-  adapter: deno(),
 });
