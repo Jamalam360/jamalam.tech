b328465d1104559b07727eb33756e2799a8814d1
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-10-12 08:27:06 +0100

    feat: new integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,22 @@ import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
+import robotsTxt from "astro-robots-txt";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jamalam360.github.io/jamalam.tech",
-  integrations: [tailwind(), sitemap(), preact(), image(), prefetch()],
+  site: "https://jamalam360.github.io/",
+  base: "/jamalam.tech",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    preact(),
+    image(),
+    prefetch(),
+    robotsTxt({
+      host: "jamalam.tech",
+    }),
+    compress(),
+  ],
 });
