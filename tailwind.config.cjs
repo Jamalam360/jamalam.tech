cc2b7102bff5f7f4b6a5153e3816d17eb4e6393d
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-10-12 19:38:54 +0100

    feat: more frameworking

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,13 +7,13 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
         jetbrains: ["JetBrains Mono", "monospace"],
       },
+      dropShadow: {
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
+      },
     },
   },
-  safelist: [
-    "bg-blue-400",
-    "bg-blue-700",
-    "bg-yellow-500",
-    "bg-orange-500",
-  ],
   plugins: [],
 };
