3b96e3ad2d96bfe3557a0646d9bece864d810084
Jamalam <james@jamalam.tech>
2025-10-10 13:22:45 +0100

    feat: 2025 updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [],
   redirects: {
     "/discord": "https://discord.gg/88PWg5TySd",
   }
